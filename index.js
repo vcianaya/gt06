a7f751c54c64d4694dce94194593f51dc537226c
unknown <vcianya>
2018-01-06 19:03:36 -0400

    enviars

--- index.js
+++ index.js
@@ -6,44 +6,32 @@ var options = {
     'device_adapter'        : "GT06"
 }
 
-var server = gps.server(options,function(device,connection){
+var options = {
+    debug: true,
+    port: 8090,
+    device_adapter: 'TK103B'
+}
 
-    device.on("connected",function(data){
+var server = gps.server(options, function (device, connection) {
 
-        console.log("I'm a new device connected");
-        return data;
+    device.on('login_request', function (device_id, msg_parts) {
+        // Some devices sends a login request before transmitting their position
+        // Do some stuff before authenticate the device...
 
-    });
+        // Accept the login request. You can set false to reject the device.
+        this.login_authorized(true)
 
-    device.on("login_request",function(device_id,msg_parts){
-
-        console.log('Hey! I want to start transmiting my position. Please accept me. My name is '+device_id);
-
-        this.login_authorized(true); 
-
-        console.log("Ok, "+device_id+", you're accepted!");
-
-    });
-    
-
-    device.on("ping",function(data){
-        //this = device
-        console.log("I'm here: "+data.latitude+", "+data.longitude+" ("+this.getUID()+")");
-
-        //Look what informations the device sends to you (maybe velocity, gas level, etc)
-        //console.log(data);
-        return data;
-
-    });
-
-   device.on("alarm",function(alarm_code,alarm_data,msg_data){
-        console.log("Help! Something happend: "+alarm_code+" ("+alarm_data.msg+")");
-    }); 
-
-    //Also, you can listen on the native connection object
-    connection.on('data',function(data){
-        //echo raw data package
-        //console.log(data.toString()); 
     })
 
+    //PING -> When the gps sends their position
+    device.on('ping', function (data) {
+
+        //After the ping is received, but before the data is saved
+        //console.log(data);
+        return data
+
+    });
+
 });
+
+server.setDebug(true);

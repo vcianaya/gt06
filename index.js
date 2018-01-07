8ab8f132ccb73c86779df05edb7fff3b51b6b4ef
unknown <vcianya>
2018-01-06 21:00:25 -0400

    ENVIaR

--- index.js
+++ index.js
@@ -1,37 +1,49 @@
 var gps = require("gps-tracking");
 
 var options = {
-    'debug'                 : true, //We don't want to debug info automatically. We are going to log everything manually so you can check what happens everywhere
-    'port'                  : 5000,
-    'device_adapter'        : "GT06"
-}
-
-var options = {
-    debug: true,
-    port: 5000,
-    device_adapter: 'GT06'
+    'debug': false, //We don't want to debug info automatically. We are going to log everything manually so you can check what happens everywhere
+    'port': 8090,
+    'device_adapter': "TK103"
 }
 
 var server = gps.server(options, function (device, connection) {
 
-    device.on('login_request', function (device_id, msg_parts) {
-        // Some devices sends a login request before transmitting their position
-        // Do some stuff before authenticate the device...
+    device.on("connected", function (data) {
 
-        // Accept the login request. You can set false to reject the device.
-        this.login_authorized(true)
-
-    })
-
-    //PING -> When the gps sends their position
-    device.on('ping', function (data) {
-
-        //After the ping is received, but before the data is saved
-        //console.log(data);
-        return data
+        console.log("I'm a new device connected");
+        return data;
 
     });
 
-});
+    device.on("login_request", function (device_id, msg_parts) {
 
-server.setDebug(true);
+        console.log('Hey! I want to start transmiting my position. Please accept me. My name is ' + device_id);
+
+        this.login_authorized(true);
+
+        console.log("Ok, " + device_id + ", you're accepted!");
+
+    });
+
+
+    device.on("ping", function (data) {
+        //this = device
+        console.log("I'm here: " + data.latitude + ", " + data.longitude + " (" + this.getUID() + ")");
+
+        //Look what informations the device sends to you (maybe velocity, gas level, etc)
+        //console.log(data);
+        return data;
+
+    });
+
+    device.on("alarm", function (alarm_code, alarm_data, msg_data) {
+        console.log("Help! Something happend: " + alarm_code + " (" + alarm_data.msg + ")");
+    });
+
+    //Also, you can listen on the native connection object
+    connection.on('data', function (data) {
+        //echo raw data package
+        //console.log(data.toString()); 
+    })
+
+});
